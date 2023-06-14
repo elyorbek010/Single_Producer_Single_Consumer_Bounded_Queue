54daf2f2db2ebceaee6d413e6959bd89ae5e497a
Elyorbek Kholikov <elyorbek4111020@list.ru>
2023-06-14 18:37:28 +0000

    refactor: Optimize memory order

--- spsc/vector.c
+++ spsc/vector.c
@@ -1,19 +1,19 @@
 /*
-* SPSC Bounded Vector.
-*
-* Vector's working principles:
-* Vector of size 3
-*   |1|2|3|.|
-*    |     |
-* [begin  end)
-*
-* Begin index is inclusive, the element at that index exists
-* End index is exclusive, the element at that index does not exist
-*
-* Corner cases:
-*	'begin == end' -- vector is empty, because 'end' is exclusive
-*	'next(end) == begin' -- vector is full, because 'end' catched up 'begin
-*/
+ * SPSC Bounded Vector.
+ *
+ * Vector's working principles:
+ * Vector of size 3
+ *   |1|2|3|.|
+ *    |     |
+ * [begin  end)
+ *
+ * Begin index is inclusive, the element at that index exists
+ * End index is exclusive, the element at that index does not exist
+ *
+ * Corner cases:
+ *	'begin == end' -- vector is empty, because 'end' is exclusive
+ *	'next(end) == begin' -- vector is full, because 'end' catched up 'begin
+ */
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -23,47 +23,48 @@
 #include "vector.h"
 #include "debug.h"
 
-#define CHECK_AND_RETURN_IF_NOT_EXIST(pointer_object)  \
-    do{                                                \
-        if (pointer_object == NULL)                    \
-        {                                              \
-            debug_print("Object does not exist\n");    \
-            return VECTOR_FAILURE;                     \
-        }                                              \
-    }while(0);
+#define CHECK_AND_RETURN_IF_NOT_EXIST(pointer_object) \
+	do                                                \
+	{                                                 \
+		if (pointer_object == NULL)                   \
+		{                                             \
+			debug_print("Object does not exist\n");   \
+			return VECTOR_FAILURE;                    \
+		}                                             \
+	} while (0);
 
 struct vector_t
 {
 	size_t capacity;
-	atomic_size_t begin;		// begin index is inclusive
-	atomic_size_t end;			// end index is exclusive
+	atomic_size_t begin; // begin index is inclusive
+	atomic_size_t end;	 // end index is exclusive
 
-	void* element[];
+	void *element[];
 };
 
 /*
-* FUNCTION DECLARATIONS
-*/
+ * FUNCTION DECLARATIONS
+ */
 
-vector_t* vector_create(const size_t capacity);
-vector_ret_t vector_destroy(vector_t* vector);
+vector_t *vector_create(const size_t capacity);
+vector_ret_t vector_destroy(vector_t *vector);
 
-vector_ret_t vector_push(vector_t* vector, void* element);
+vector_ret_t vector_push(vector_t *vector, void *element);
 
-vector_ret_t vector_pop(vector_t* vector, void** element);
+vector_ret_t vector_pop(vector_t *vector, void **element);
 
 static inline size_t vector_next_index(const size_t index, const size_t capacity);
 static inline size_t vector_prev_index(const size_t index, const size_t capacity);
 
 /*
-* FUNCTION DEFINITIONS
-*/
+ * FUNCTION DEFINITIONS
+ */
 
-vector_t* vector_create(size_t capacity)
+vector_t *vector_create(size_t capacity)
 {
-	vector_t* vector = malloc(sizeof(*vector) + (capacity + 1) * sizeof(vector->element[0]));
+	vector_t *vector = malloc(sizeof(*vector) + (capacity + 1) * sizeof(vector->element[0]));
 
-	if (vector == NULL)		// condition that malloc() failed
+	if (vector == NULL) // condition that malloc() failed
 	{
 		debug_print("Not enough memory for capacity: %zu\n", capacity);
 		return NULL;
@@ -73,11 +74,11 @@ vector_t* vector_create(size_t capacity)
 
 	atomic_init(&vector->begin, 0);
 	atomic_init(&vector->end, 0);
-	
+
 	return vector;
 }
 
-vector_ret_t vector_destroy(vector_t* vector)
+vector_ret_t vector_destroy(vector_t *vector)
 {
 	CHECK_AND_RETURN_IF_NOT_EXIST(vector);
 
@@ -86,39 +87,41 @@ vector_ret_t vector_destroy(vector_t* vector)
 	return VECTOR_SUCCESS;
 }
 
-vector_ret_t vector_push(vector_t* vector, void* element)
+vector_ret_t vector_push(vector_t *vector, void *element)
 {
 	CHECK_AND_RETURN_IF_NOT_EXIST(vector);
-	
-	size_t begin = vector->begin;
-	size_t end = vector->end;
+
+	size_t begin = atomic_load_explicit(&vector->begin, memory_order_acquire);
+	size_t end = atomic_load_explicit(&vector->end, memory_order_acquire);
 
 	size_t next_end = vector_next_index(end, vector->capacity);
 
 	// Check if vector is FULL
-	if(next_end == begin)
+	if (next_end == begin)
 		return VECTOR_OVERFLOW;
 
 	vector->element[end] = element;
-	vector->end = next_end;
+	atomic_store_explicit(&vector->end, next_end, memory_order_release);
 
 	return VECTOR_SUCCESS;
 }
 
-vector_ret_t vector_pop(vector_t* vector, void** p_element)
+vector_ret_t vector_pop(vector_t *vector, void **p_element)
 {
 	CHECK_AND_RETURN_IF_NOT_EXIST(vector);
 	CHECK_AND_RETURN_IF_NOT_EXIST(p_element);
 
-	size_t begin = vector->begin;
-	size_t end = vector->end;
+	size_t begin = atomic_load_explicit(&vector->begin, memory_order_acquire);
+	size_t end = atomic_load_explicit(&vector->end, memory_order_acquire);
 
 	// Check if vector is EMPTY
 	if (begin == end)
 		return VECTOR_UNDERFLOW;
 
-    *p_element = vector->element[begin];
-	vector->begin = vector_next_index(begin, vector->capacity);
+	*p_element = vector->element[begin];
+	atomic_store_explicit(&vector->begin,
+						  vector_next_index(begin, vector->capacity),
+						  memory_order_release);
 
 	return VECTOR_SUCCESS;
 }
