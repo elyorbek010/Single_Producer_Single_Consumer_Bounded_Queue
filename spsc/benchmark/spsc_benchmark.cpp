a453963cc11b5d87e0548a502a6cb1271e9a1662
Elyorbek Kholikov <elyorbek4111020@list.ru>
2023-06-21 00:40:25 +0000

    benchmark: Add Logging to operations

--- spsc/benchmark/spsc_benchmark.cpp
+++ spsc/benchmark/spsc_benchmark.cpp
@@ -5,6 +5,12 @@ extern "C"
 
 #include <benchmark/benchmark.h>
 #include <thread>
+#include <iostream>
+#include <fstream>
+#include <chrono>
+#include <string>
+
+#define LOG_ENABLED 0
 
 void spsc_simulate(size_t vector_size, size_t data_amount)
 {
@@ -14,23 +20,55 @@ void spsc_simulate(size_t vector_size, size_t data_amount)
                               {
                                 vector_ret_t ret = VECTOR_SUCCESS;
                                 size_t iter = 0;
+#if LOG_ENABLED == 1
+                                std::string filename = "producer_logs_" + std::to_string(data_amount) + ".txt";
 
+                                std::ofstream producer_logs;
+
+                                producer_logs.open(filename, std::ios::out | std::ios::trunc);
+                                if (!producer_logs.is_open())
+                                {
+                                  std::cout << "error opening" << filename << std::endl;
+                                }
+#endif
                                 while (iter < data_amount)
                                 {
                                   ret = vector_push(vector, (void *)iter);
 
                                   if (ret == VECTOR_FAILURE)
                                     abort();
-                                  else if (ret == VECTOR_SUCCESS) 
+                                  else if (ret == VECTOR_SUCCESS)
+                                  {
+#if LOG_ENABLED == 1
+                                    producer_logs << "PUSHED " << iter << " " << std::chrono::system_clock::now().time_since_epoch() / std::chrono::nanoseconds(1) << "\n";
+#endif
                                     iter++;
-                                } });
+                                  }
+                                  else if (ret == VECTOR_OVERFLOW)
+                                  {
+#if LOG_ENABLED == 1
+                                    producer_logs << "OVERFLOW " << std::chrono::system_clock::now().time_since_epoch() / std::chrono::nanoseconds(1) << "\n";
+#endif
+                                    // continue loop, try to push data
+                                  }
+                                }
+
+#if LOG_ENABLED == 1
+                                producer_logs.close();
+#endif
+                              });
 
   auto consumer = std::thread([=]()
                               {
                                 vector_ret_t ret = VECTOR_SUCCESS;
                                 void *data_ptr = nullptr;
                                 size_t iter = 0;
+#if LOG_ENABLED == 1
+                                std::string filename = "consumer_logs_" + std::to_string(data_amount) + ".txt";
 
+                                std::ofstream consumer_logs;
+                                consumer_logs.open(filename, std::ios::out | std::ios::trunc);
+#endif
                                 while (iter < data_amount)
                                 {
                                   ret = vector_pop(vector, &data_ptr);
@@ -38,8 +76,24 @@ void spsc_simulate(size_t vector_size, size_t data_amount)
                                   if (ret == VECTOR_FAILURE)
                                     abort();
                                   else if (ret == VECTOR_SUCCESS)
+                                  {
+#if LOG_ENABLED == 1
+                                    consumer_logs << "POPPED " << (size_t)data_ptr << std::chrono::system_clock::now().time_since_epoch() / std::chrono::nanoseconds(1) << "\n";
+#endif
                                     iter++;
-                                } });
+                                  }
+                                  else if (ret == VECTOR_UNDERFLOW)
+                                  {
+#if LOG_ENABLED == 1
+                                    consumer_logs << "UNDERFLOW " << std::chrono::system_clock::now().time_since_epoch() / std::chrono::nanoseconds(1) << "\n";
+#endif
+                                    // continue loop, try to pop data
+                                  }
+                                }
+#if LOG_ENABLED == 1
+                                consumer_logs.close();
+#endif
+                              });
 
   // join threads
   producer.join();
